9c879eda7466f75c565195c3f5ad93292e0b93ab
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2018-01-04 22:32:54 +0900

    Create a test for dynamoDBClient.ts

---
+++ server/__tests__/db/dynamoDBClient.ts
@@ -0,0 +1,25 @@
+import { getClient, disconnect } from '../../db/dynamoDBClient';
+
+describe('db/dynamoDBClient', () => {
+  afterEach(() => {
+    disconnect();
+  });
+
+  describe('When process.env.IS_OFFLINE is true', () => {
+    it('should set localhost as a region.', () => {
+      process.env.IS_OFFLINE = 'true';
+      const dynamoDB = getClient();
+      expect(dynamoDB.options.endpoint).toEqual('http://localhost:8000');
+      expect(dynamoDB.options.region).toEqual('localhost');
+    });
+  });
+
+  describe('When process.env.IS_OFFLINE is false', () => {
+    it('should set a as a region.', () => {
+      process.env.IS_OFFLINE = 'false';
+      const dynamoDB = getClient();
+      expect(dynamoDB.options.endpoint).toEqual(undefined);
+      expect(dynamoDB.options.region).toEqual(undefined);
+    });
+  });
+});
--- server/db/dynamoDBClient.ts
+++ server/db/dynamoDBClient.ts
@@ -1,14 +1,29 @@
 import * as AWS from 'aws-sdk';
 
-const IS_OFFLINE = process.env.IS_OFFLINE;
-let dynamoDBClient: AWS.DynamoDB.DocumentClient;
-if (IS_OFFLINE === 'true') {
-  dynamoDBClient = new AWS.DynamoDB.DocumentClient({
-    endpoint: 'http://localhost:8000',
-    region: 'localhost',
-  });
-} else {
-  dynamoDBClient = new AWS.DynamoDB.DocumentClient();
-}
+// Singleton
+let dynamoDBClient: AWS.DynamoDB.DocumentClient | undefined;
+const getClient = () : AWS.DynamoDB.DocumentClient  => {
+  if(dynamoDBClient) {
+    return dynamoDBClient;
+  }
 
-export default dynamoDBClient;
+  // process.env.IS_OFFLINE is set by `serverless-offline` (a plugin.)
+  const IS_OFFLINE = process.env.IS_OFFLINE;
+  if (IS_OFFLINE === 'true') {
+    dynamoDBClient = new AWS.DynamoDB.DocumentClient({
+      endpoint: 'http://localhost:8000',
+      region: 'localhost',
+    });
+  } else {
+    dynamoDBClient = new AWS.DynamoDB.DocumentClient();
+  }
+
+  return dynamoDBClient;
+};
+
+const disconnect = () => {
+  dynamoDBClient = undefined;
+};
+
+
+export { getClient, disconnect };
